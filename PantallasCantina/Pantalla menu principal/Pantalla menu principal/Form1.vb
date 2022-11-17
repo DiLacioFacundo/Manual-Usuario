f64314967cd36ad5c62fe9380eacb456fd052070
ValenBentos <88683261+ValenBentos@users.noreply.github.com>
2022-11-17 09:50:45 -0300

    Add files via upload

---
+++ PantallasCantina/Pantalla menu principal/Pantalla menu principal/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
